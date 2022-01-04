7baec2cf1ff48556cbe8fb38b58fee32f0bc3083
daves <davidlimjh77@gmail.com>
2022-01-04 21:45:58 +0800

    tailwind config

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.get("/", (req, res) => res.render("boards"));
 
 // https://github.com/FoongL/in-class-express
 app.listen(PORT, () => {
